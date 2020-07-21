37f46bb01cf0971c190c6d62df1d0d270867d517
Rajath-S <rajaths2510@gmail.com>
2020-07-21 14:45:52 +0000

    stl runs

--- data_loader.py
+++ data_loader.py
@@ -27,8 +27,8 @@ def get_train_valid_loader(data_dir,
         dataset = datasets.CIFAR10(data_dir, train=True, download=True,
                 transform=transforms.Compose(trans))
     elif dataset=="stl10":
-      trans = [transforms.ToTensor(),
-              transforms.Resize(32),
+      trans = [transforms.Resize(32),
+                  transforms.ToTensor(),  
                  transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
       dataset=datasets.STL10(data_dir,
                               split='train',
@@ -128,7 +128,7 @@ def get_test_loader(data_dir,
                                 ]))
     elif dataset=="stl10":
         trans = [transforms.ToTensor(),
-                transforms.Resize((32,32)),
+                transforms.Resize(32),
                  transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
         dataset=datasets.STL10(data_dir,split='test',transform=transforms.Compose(trans),download=True)
 
@@ -157,7 +157,7 @@ DATASET_CONFIGS = {
     'svhn': {'size': 32, 'channels': 3, 'classes': 10},
     'smallnorb': {'size': 32, 'channels': 1, 'classes': 5},
     'mnist':{'size':28, 'channels':1, 'classes':10},
-    'stl10':{'size':96, 'channels': 3, 'classes': 10},
+    'stl10':{'size':32, 'channels': 3, 'classes': 10},
 }
 
 VIEWPOINT_EXPS = ['azimuth', 'elevation']
